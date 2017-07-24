8ab0e4f343d37a8f60bb258f402b7385364894bc
Bhargava Peddapudi <bpeddapudi@equinix.com>
2017-07-24 16:57:18 -0700

    Integrated basic google maps

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 $( document ).ready(function(){
    $(".button-collapse").sideNav();
    $('.main-carousel').carousel({fullWidth: true});
+   $('.modal').modal();
+
+   var teamMember = [
+     "Vivek Kumar",
+     "Chihiro Jameson",
+     "Dilip Sundarraj"
+   ];
+   var teamMemberDp = [
+     "img/team/vk.jpg",
+     "img/team/cj.jpg",
+     "img/team/ds.jpg"
+   ];
+   var memberDetails = [
+     "Vivek is a firm believer in the power of education as a tool to empower children. As a native of India, Vivek is familiar with the culture and history of India and is committed to EmpowerKids Foundation's activities in India. Vivek holds a Bachelor's degree in Computer Science and Engineering from the National Institute of Technology, Jamshedpur. Vivek is a Software Executive with Equinix and an Executive Committee Member of MIT/Stanford Venture Lab.",
+     "Chihiro has extensive experience in managing nonprofit organizations and in providing educational resources to youths. Prior to working at transactional groups of international law firms in Silicon Valley, she taught creative writing, oratory, and test-taking skills to high school and middle school students through various youth empowerment programs. Chihiro holds two bachelor's degrees and a master's degree from Stanford University, and is on the Board of Directors of MIT/Stanford Venture Lab and SV Links.",
+     "Dilip is a Silicon Valley engineer, technologist and an aspiring entrepreneur. He has been an active program volunteer with other non-profit organizations in the past. Dilip heads the outreach team at MIT/Stanford Venture Lab and holds both bachelor's and master's degrees in Computer Science. Empowering underprivileged children through education is his passion. He enjoys playing basketball and golf."
+   ]
+
 });
