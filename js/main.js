fec5d479088f58d4272595858bf58ecfa159e71c
Bhargava Peddapudi <bpeddapudi@equinix.com>
2017-09-06 17:42:08 -0700

    Feedback changes added to the site.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ $( document ).ready(function(){
    $(".button-collapse").sideNav();
    $('.main-carousel').carousel({fullWidth: true});
    $('.main-carousel').carousel('set', 1);
+   $('.carousel.carousel-slider').carousel({fullWidth: true});
+   autoplay();
+   function autoplay() {
+        $('.carousel.carousel-slider').carousel('next');
+        setTimeout(autoplay, 6000);
+    }
    $('.modal').modal();
 
    /******************** scroll to ************************/
@@ -44,12 +50,12 @@ $( document ).ready(function(){
    var teamMember = [
      "Vivek Kumar",
      "Chihiro Jameson",
-     "Dilip Sundarraj"
+     "Puja Venkataraman"
    ];
    var memberDetails = [
      "Vivek is a firm believer in the power of education as a tool to empower children. As a native of India, Vivek is familiar with the culture and history of India and is committed to EmpowerKids Foundation's activities in India. Vivek holds a Bachelor's degree in Computer Science and Engineering from the National Institute of Technology, Jamshedpur. Vivek is a Software Executive with Equinix and an Executive Committee Member of MIT/Stanford Venture Lab.",
      "Chihiro has extensive experience in managing nonprofit organizations and in providing educational resources to youths. Prior to working at transactional groups of international law firms in Silicon Valley, she taught creative writing, oratory, and test-taking skills to high school and middle school students through various youth empowerment programs. Chihiro holds two bachelor's degrees and a master's degree from Stanford University, and is on the Board of Directors of MIT/Stanford Venture Lab and SV Links.",
-     "Dilip is a Silicon Valley engineer, technologist and an aspiring entrepreneur. He has been an active program volunteer with other non-profit organizations in the past. Dilip heads the outreach team at MIT/Stanford Venture Lab and holds both bachelor's and master's degrees in Computer Science. Empowering underprivileged children through education is his passion. He enjoys playing basketball and golf."
+     "Puja is an HR Professional and enjoys to volunteer in meaningful non-profit organizations. Empowering underprivileged children through education is Puja's passion. She will be helping on opportunities to spread our initiatives and goals to help raise funds, and donations to help our children gain scholarships and tuition.Her deep interest and concern for meaningful causes, span from heading and participating as a team lead for fund raising for organizations such as Autism Speaks, American Cancer Society etc.  Puja comes with a Bachelor Degree from Les Roches, Siwtzlernd in International Hospitality Management."
    ]
    $(".modal-trigger").click(function(){
      var teamateIndexIn = $(this).attr("data-index");
